d4788b733308a0a109063d18116a94775233f94e
nyix <rsa17826@email.vccs.edu>
2026-09-11 20:36:05 -0400

    NO MESSAGE SET

--- argtree.go
+++ argtree.go
@@ -322,12 +322,15 @@ func describePossibility(p ArgPossibility) string {
 				parts = append(parts, fmt.Sprintf("%s<%s%s%s>%s", colorSep, colorTypeName, p.Type.Name, colorSep, ansiReset))
 			}
 			if len(values) == 1 {
-				parts = append(parts, fmt.Sprintf("%skeyword%s:%s %s%s", colorTypeName, colorSep, colorValues, values[0], ansiReset))
+				parts = append(parts, fmt.Sprintf("%s:%s %s%s", colorSep, colorValues, values[0], ansiReset))
+				// parts = append(parts, fmt.Sprintf("%skeyword%s:%s %s%s", colorTypeName, colorSep, colorValues, values[0], ansiReset))
 			} else if len(values) > maxHelpListItems {
 				shown := strings.Join(values[:maxHelpListItems], joiner)
-				parts = append(parts, fmt.Sprintf("%sone of%s:%s %s%s, ...%s (%d total)%s", colorTypeName, colorSep, colorValues, shown, colorSep, colorValues, len(values), ansiReset))
+				parts = append(parts, fmt.Sprintf("%s:%s %s%s, ...%s (%d total)%s", colorSep, colorValues, shown, colorSep, colorValues, len(values), ansiReset))
+				// parts = append(parts, fmt.Sprintf("%sone of%s:%s %s%s, ...%s (%d total)%s", colorTypeName, colorSep, colorValues, shown, colorSep, colorValues, len(values), ansiReset))
 			} else {
-				parts = append(parts, fmt.Sprintf("%sone of%s:%s %s%s", colorTypeName, colorSep, colorValues, strings.Join(values, joiner), ansiReset))
+				parts = append(parts, fmt.Sprintf("%s:%s %s%s", colorSep, colorValues, strings.Join(values, joiner), ansiReset))
+				// parts = append(parts, fmt.Sprintf("%sone of%s:%s %s%s", colorTypeName, colorSep, colorValues, strings.Join(values, joiner), ansiReset))
 			}
 		}
 	}
